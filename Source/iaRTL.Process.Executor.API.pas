50dc938cca4846c18ac27cec403ede5be7e8af94
Darian Miller <darian@darianmiller.com>
2019-12-26 21:57:13 -0600

    Further refactoring via IAProcessExecutor interface

---
+++ Source/iaRTL.Process.Executor.API.pas
@@ -0,0 +1,36 @@
+{
+  Copyright 2019 Ideas Awakened Inc.
+  Part of the "iaLib" shared code library for Delphi
+  For more detail, see: https://github.com/ideasawakened/iaLib
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+
+Module History
+1.0 2019-Dec-26 Darian Miller: Unit created
+}
+unit iaRTL.Process.Executor.API;
+
+interface
+
+type
+
+  IAProcessExecutor = interface
+    ['{EB8A7F0E-0C71-4B0B-AC1C-83C26CDDBE20}']
+
+    function LaunchProcess(const pCommandLine:string):Boolean;
+  end;
+
+implementation
+
+end.
--- Source/iaRTL.Process.Executor.Windows.pas
+++ Source/iaRTL.Process.Executor.Windows.pas
@@ -1,5 +1,7 @@
-(*
-Copyright 2019 Ideas Awakened Inc.
+{
+  Copyright 2019 Ideas Awakened Inc.
+  Part of the "iaLib" shared code library for Delphi
+  For more detail, see: https://github.com/ideasawakened/iaLib
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -13,27 +15,43 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 
-For more detail, see: https://github.com/ideasawakened/iaLib
 
+Module History
 1.1 2019-Dec-26 Darian Miller: ASL-2.0 applied, refactored
 1.0 2019-Dec-26 Darian Miller: Unit created using Delphi Dabbler's Public Domain code.
-*)
+}
 unit iaRTL.Process.Executor.Windows;
 
 interface
+uses
+  iaRTL.Process.Executor.API;
+
+type
+
+  TiaWindowsProcessExecutor = class(TInterfacedObject, IAProcessExecutor)
+  public
+    {IAProcessExecutor}
+    /// <summary>
+    /// Executes the given command line and waits for the program started by the
+    /// command line to exit. Returns true if the program returns a zero exit code and
+    /// false if the program doesn't start or returns a non-zero error code.
+    /// </summary>
+    function LaunchProcess(const pCommandLine:string):Boolean;
+  end;
 
   /// <summary>
   /// Executes the given command line and waits for the program started by the
   /// command line to exit. Returns true if the program returns a zero exit code and
   /// false if the program doesn't start or returns a non-zero error code.
   /// </summary>
-  function ExecAndWait(const aCommandLine:string):Boolean;
+  function ExecAndWait(const pCommandLine:string):Boolean;
 
 implementation
 uses
   WinAPI.Windows;
 
-function ExecAndWait(const aCommandLine:string):Boolean;
+
+function TiaWindowsProcessExecutor.LaunchProcess(const pCommandLine:string):Boolean;
 var
   vStartupInfo:TStartupInfo;
   vProcessInfo:TProcessInformation;
@@ -43,7 +61,7 @@ begin
   Result := False;
 
   // Modification to work round "feature" in CreateProcessW API function used by Unicode Delphis.
-  vSafeCommandLine := aCommandLine;
+  vSafeCommandLine := pCommandLine;
   UniqueString(vSafeCommandLine);
 
   FillChar(vStartupInfo, SizeOf(vStartupInfo), 0);
@@ -67,6 +85,18 @@ begin
     end;
   end;
   //todo: GetLastError
+end;
+
+function ExecAndWait(const pCommandLine:string):Boolean;
+var
+  vProcessLauncher:TiaWindowsProcessExecutor;
+begin
+  vProcessLauncher := TiaWindowsProcessExecutor.Create;
+  try
+    Result := vProcessLauncher.LaunchProcess(pCommandLine);
+  finally
+    vProcessLauncher.Free;
+  end;
 end;
 
 end.
