062e54a48f1e92bfab00fbea57a3a5e6c29386e0
Darian Miller <darian@darianmiller.com>
2023-04-29 12:54:40 -0500

    Change license to Apache

--- Source/iaRTL.Thread.pas
+++ Source/iaRTL.Thread.pas
@@ -1,24 +1,24 @@
-(*
-Copyright (c) 2016 Darian Miller
-All rights reserved.
+{
+  Copyright 2023 Ideas Awakened Inc.
+  Part of the "iaLib" shared code library for Delphi
+  For more detail, see: https://github.com/ideasawakened/iaLib
 
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, and/or sell copies of the
-Software, and to permit persons to whom the Software is furnished to do so, provided that the above copyright notice(s) and this permission notice
-appear in all copies of the Software and that both the above copyright notice(s) and this permission notice appear in supporting documentation.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT OF THIRD PARTY RIGHTS. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR HOLDERS INCLUDED IN THIS NOTICE BE
-LIABLE FOR ANY CLAIM, OR ANY SPECIAL INDIRECT OR CONSEQUENTIAL DAMAGES, OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER
-IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+   http://www.apache.org/licenses/LICENSE-2.0
 
-Except as contained in this notice, the name of a copyright holder shall not be used in advertising or otherwise to promote the sale, use or other
-dealings in this Software without prior written authorization of the copyright holder.
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
 
-As of January 2016, latest version available online at:
-  https://github.com/darianmiller/dxLib
-*)
 
+Module History
+  2.0 2023-04-29 Darian Miller: Migrated from old D5-compatible DXLib to iaLib for future work
+}
 unit iaRTL.Thread;
 
 interface
