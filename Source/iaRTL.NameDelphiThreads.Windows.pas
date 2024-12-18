96c4b4d5801d358a70e7c141e646811cee34d58a
Darian Miller <darian@darianmiller.com>
2024-12-17 22:10:08 -0600

    V2 - support the Get/SetThreadDescription routines available in Windows 10, 1607 and later + still set TThread.NameThreadForDebugging used by the RAD Studio IDE + Delay call to ensure the Delphi created threads have started.

--- Source/iaRTL.NameDelphiThreads.Windows.pas
+++ Source/iaRTL.NameDelphiThreads.Windows.pas
@@ -26,7 +26,7 @@ interface
 uses
   WinAPI.Windows;
 
-  procedure NameDelphiThreads(const MainThreadId:THandle);
+  procedure NameDelphiThreads;
 
 implementation
 uses
@@ -34,15 +34,47 @@ uses
   System.Classes,
   WinAPI.TlHelp32;
 
+type
+  TGetThreadDescription = function(hThread: THandle; var ppszThreadDescription: PWideChar): HRESULT; stdcall;
+  TSetThreadDescription = function(hThread: THandle; lpThreadDescription: PWideChar): HRESULT; stdcall;
 
-procedure NameDelphiThreads(const MainThreadId:THandle);
+  function OpenThread(dwDesiredAccess: DWORD; bInheritHandle: BOOL; dwThreadId: DWORD): THandle; stdcall; external 'kernel32.dll';
+
+const //from winnt.h
+  THREAD_TERMINATE                 = $0001;
+  THREAD_SUSPEND_RESUME            = $0002;
+  THREAD_GET_CONTEXT               = $0008;
+  THREAD_SET_CONTEXT               = $0010;
+  THREAD_QUERY_INFORMATION         = $0040;
+  THREAD_SET_INFORMATION           = $0020;
+  THREAD_SET_THREAD_TOKEN          = $0080;
+  THREAD_IMPERSONATE               = $0100;
+  THREAD_DIRECT_IMPERSONATION      = $0200;
+  THREAD_SET_LIMITED_INFORMATION   = $0400;  // winnt
+  THREAD_QUERY_LIMITED_INFORMATION = $0800;  // winnt
+  THREAD_RESUME                    = $1000;  // winnt
+  THREAD_ALL_ACCESS = STANDARD_RIGHTS_REQUIRED or SYNCHRONIZE or $FFFF;
+
+
+procedure NameDelphiThreads;
 var
   hSnapshot:THandle;
   hProcessId:THandle;
+  hThread:THandle;
+  hKernel32:THandle;
   ThreadDetails:TThreadEntry32;
   i:Integer;
+  GetThreadDescription:TGetThreadDescription;
+  SetThreadDescription:TSetThreadDescription;
+  pDescription:PWideChar;
+  ResultCode:HRESULT;
+  ThreadName:String;
 begin
   hProcessId := GetCurrentProcessId;
+  hKernel32 := GetModuleHandle('Kernel32.dll');
+  @GetThreadDescription := GetProcAddress(hKernel32, 'GetThreadDescription');
+  @SetThreadDescription := GetProcAddress(hKernel32, 'SetThreadDescription');
+
 
   hSnapshot := CreateToolhelp32Snapshot(TH32CS_SNAPTHREAD, hProcessId);
   if hSnapshot <> INVALID_HANDLE_VALUE then
@@ -56,14 +88,47 @@ begin
         repeat
           if ThreadDetails.th32OwnerProcessID = hProcessId then
           begin
-            if ThreadDetails.th32ThreadID = MainThreadId then
+            if Assigned(GetThreadDescription) and Assigned(SetThreadDescription) then //Windows 10, version 1607 or later; Windows Server 2016; Windows 10 LTSB 2016
             begin
-              TThread.NameThreadForDebugging('DelphiCreated_Main', MainThreadId);
+              hThread := OpenThread(THREAD_QUERY_INFORMATION or THREAD_SET_LIMITED_INFORMATION, False, ThreadDetails.th32ThreadID);
+              if hSnapshot <> INVALID_HANDLE_VALUE then
+              begin
+                ResultCode := GetThreadDescription(hThread, pDescription);
+                if Succeeded(ResultCode) and Assigned(pDescription) then
+                begin
+                  try
+                    if Length(pDescription) = 0 then
+                    begin
+                      if ThreadDetails.th32ThreadID = MainThreadId then
+                      begin
+                        ThreadName := 'DelphiCreated_MainThread';
+                        SetThreadDescription(hThread, PWideChar(ThreadName));
+                        TThread.NameThreadForDebugging(ThreadName, MainThreadId);
+                      end
+                      else
+                      begin
+                        ThreadName := 'DelphiCreated_Worker';
+                        SetThreadDescription(hThread, PWideChar(ThreadName));
+                        TThread.NameThreadForDebugging(ThreadName, ThreadDetails.th32ThreadID);
+                      end;
+                    end;
+                  finally
+                    LocalFree(HLOCAL(pDescription));
+                  end;
+                end;
+              end;
             end
             else
             begin
-              TThread.NameThreadForDebugging('DelphiCreated_' + IntToStr(i), ThreadDetails.th32ThreadID);
-              Inc(i);
+              if ThreadDetails.th32ThreadID = MainThreadId then
+              begin
+                TThread.NameThreadForDebugging('DelphiCreated_Main', MainThreadId);
+              end
+              else
+              begin
+                TThread.NameThreadForDebugging('DelphiCreated_' + IntToStr(i), ThreadDetails.th32ThreadID);
+                Inc(i);
+              end;
             end;
           end;
         until not Thread32Next(hSnapshot, ThreadDetails);
@@ -76,7 +141,11 @@ end;
 
 {$IFDEF DEBUG}
 initialization
-  NameDelphiThreads(GetCurrentThreadId);
+TThread.ForceQueue(nil,
+    procedure
+    begin
+      NameDelphiThreads;
+    end, 100);
 {$ENDIF}
 
 end.
